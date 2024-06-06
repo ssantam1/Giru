a060a6af3fe05fd37536d32b810ff2f47572961b
steven santamorena <stevenasanta@gmail.com>
2024-06-06 01:34:36 -0400

    Add foundation for database

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const fs = require('node:fs');
 const path = require('node:path');
+const Sequelize = require('sequelize');
 const { Client, Collection, GatewayIntentBits, Partials } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({
 	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
 	partials: [Partials.Message, Partials.Reaction],
+});
+
+const sequelize = new Sequelize('database', 'user', 'password', {
+	host: 'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	storage: 'database.sqlite',
 });
 
 client.commands = new Collection();
